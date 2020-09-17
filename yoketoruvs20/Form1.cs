7574fea19053d534d46b4b927f9ba9e69aab5bdf
Yu Tanaka <YRK00337@nifty.com>
2020-09-17 11:36:30 +0900

    キャラクター定数定義

--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -15,6 +15,21 @@ namespace yoketoruvs20
     {
         const bool isDebug = true;
 
+        const int PlayerMax = 1;
+        const int EnemyMax = 10;
+        const int ItemMax = 10;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] chrs = new Label[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;
+        const int ItemIndex = EnemyIndex + EnemyMax;
+
+        const string PlayerText = "(・ω・)";
+        const string EnemyText = "◆";
+        const string ItemText = "★";
+
+        static Random rand = new Random();
+
         enum State
         {
             None = -1,  // 無効
